57d3039df0a8286774da955105b40dca20feb43d
Tony Nguyen <tntuyen@ucdavis.edu>
2017-05-01 15:29:05 -0700

    finish mobile layout

--- src/components/About.js
+++ src/components/About.js
@@ -15,29 +15,26 @@ class About extends Component {
 			<div className="stars" id="main-about">
 		<div className="twinkling">
 			<div id="content-container">
-				<div className="work-intro-content">
+				<div className="work-intro-content container">
 					<ReactCSSTransitionGroup
                 transitionName="slide"
                 transitionEnterTimeout={500}
                 transitionLeaveTimeout={500}
                 transitionAppear={true}
                 transitionAppearTimeout={500}>
-						<div className="about-header container" id="title">
+						<div className="container" id="title">
 							<div className="row">
 								<div className="col-md-12">
-							<em><h1 className="about-header" id="titlecontent"><span className="myname">About Me</span><br/><br/> I enjoy building and working on <span className="cyan">meaningful</span> things<br /><br />
-							<span id="decorate">Outside of code</span><br/><br/>Hobbies<br/><br/>
-							- Lifting<br></br> - Reading<br/>- Playing Music<br/><br/>
-
-							This is a picture of my dog, <span className="cyan">Ace</span><br/><br/>
-							Isn't he cute?
-							<br /><div id="what" className="thumbnail"><img id="ace-pic" src={ace} /></div><br/><br/></h1></em>
+									<h1 id='heading'> About Me</h1>
+									<p id="about-desc">
+										My name is <span className="cyan">Tony</span> and I'm a software developer with a <span className="cyan">passion</span> for programming. I love <span className="cyan">building</span> things for fun, but what I really enjoy is working on projects that are <span className="cyan">meaningful</span> and have a <span className="cyan">positive impact</span> on others. Outside of code, I love hanging out with my dog, lifting, reading, and playing music!
+									</p>
 								</div> 
 							</div>
 						</div>
 					</ReactCSSTransitionGroup>
 				</div>
-
+				<div className="container">
 				<div className="row">
 					<div className="col-md-12" id="empl-edu">
 						<h1 id="empl-title">What I've Accomplished </h1>
@@ -47,46 +44,48 @@ class About extends Component {
 
 				<div className="row" id="empl-section1">
 					<div className="col-md-6 about-pic">
-							<img className="about-img-tag" id="first-img" src={this.props.about[0].image} />
+						<div className="thumbnail about-thumbnails">
+							<img className="about-img-tag" src={this.props.about[0].image} />
+						</div>
 					</div>
-
 					<div className="col-md-6 about-descr">
 						<h1 id="job-title">{this.props.about[0].title}</h1>
-							<p>{this.props.about[0].desc}</p>
 					</div>
+				</div>
 
+				<div className="row" id="empl-section1">
 					<div className="col-md-6 about-pic">
+						<div className="thumbnail about-thumbnails">
 							<img className="about-img-tag" src={this.props.about[1].image} />
+						</div>
 					</div>
-
 					<div className="col-md-6 about-descr">
 						<h1 id="job-title">{this.props.about[1].title}</h1>
-							<p>{this.props.about[1].desc}</p>
 					</div>
+				</div>
 
+				<div className="row" id="empl-section1">
 					<div className="col-md-6 about-pic">
-						<div className="thumbnail">
+						<div className="thumbnail about-thumbnails">
 							<img className="about-img-tag" src={this.props.about[2].image} />
 						</div>
 					</div>
-
 					<div className="col-md-6 about-descr">
 						<h1 id="job-title">{this.props.about[2].title}</h1>
-							<p>{this.props.about[2].desc}</p>
 					</div>
+				</div>
 
+				<div className="row" id="empl-section1">
 					<div className="col-md-6 about-pic">
-						<div className="thumbnail" >
+						<div className="thumbnail about-thumbnails" >
 							<img className="about-img-tag" src={this.props.about[3].image} />
 						</div>
 					</div>
-
 					<div className="col-md-6 about-descr" id="dbc">
 						<h1 id="job-title">{this.props.about[3].title}</h1>
-							<p>{this.props.about[3].desc}</p>
 					</div>
-
 				</div>
+			</div>
 
 				<div className="app-modal" >
           <AppModal />
